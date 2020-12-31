3161b6f5cf00088f35bd3cfac7cd52446ef910c4
Johyn Papin <contact@johyn.me>
2020-12-31 10:37:25 +0100

    Added an example

---
+++ example/src/App.js
@@ -0,0 +1,20 @@
+import { Container, Typography } from "@material-ui/core";
+import { useForm } from "@ggf/ggf";
+
+function App() {
+  const { fields } = useForm({ name: "User" });
+
+  return (
+    <Container>
+      <Typography variant="h3" gutterBottom>
+        Modifier votre profil
+      </Typography>
+
+      {fields.email}
+      {fields.firstName}
+      {fields.lastName}
+    </Container>
+  );
+}
+
+export default App;
---
+++ example/src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { loader } from "graphql.macro";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+import { GGFProvider } from "@ggf/ggf";
+
+const theme = createMuiTheme({
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        margin: "normal",
+        variant: "outlined",
+        fullWidth: true,
+      },
+    },
+  },
+});
+
+const graphQLSchema = loader("./schema.gql");
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GGFProvider graphQLSchema={graphQLSchema}>
+        <CssBaseline />
+
+        <App />
+      </GGFProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
